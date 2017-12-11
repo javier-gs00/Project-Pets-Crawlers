42aaf5817d1d500d46d8aec3bdf4fbb8e5c51a09
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-11 12:59:23 -0300

    Added Pet Happy cat food crawler

--- project_pets/project_pets/settings.py
+++ project_pets/project_pets/settings.py
@@ -28,7 +28,7 @@ CONCURRENT_REQUQESTS_PER_DOMAIN = 8
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1 # 1 = 1000 ms
+DOWNLOAD_DELAY = 3 # 1 = 1000 ms
 RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
--- project_pets/project_pets/spiders/pethappy.py
+++ project_pets/project_pets/spiders/pethappy.py
@@ -3,28 +3,14 @@ import re
 from datetime import datetime
 from project_pets.items import ProjectPetsItem
 
-class PethappySpider(scrapy.Spider):
-    name = 'pethappy'
+class PethappyDogFoodSpider(scrapy.Spider):
+    name = 'pethappy_dog_food'
     allowed_domains = ['https://www.pethappy.cl']
 
     start_urls = [
-        'https://www.pethappy.cl/perros-2/alimentos?page=%s' % page for page in range(1, 3)
+        'https://www.pethappy.cl/perros-2/alimentos?page=%s' % page for page in range(1, 9)
     ]
-
-    # def parse(self, response):
-    #     for product in response.xpath('.//div[contains(@class, "in")]'):
-    #         item = ProjectPetsItem()
-
-    #         item['name'] = product.xpath('//h1//a/text()').extract()
-    #         item['href'] = product.xpath('//p//a/@href').extract()
-    #         item['price'] = product.xpath('//p[contains(@class, "precio")]/text()').extract()
-    #         item['image_href'] = product.xpath('//p//a//img/@src').extract()
-    #         item['store'] = "Pet Happy"
-    #         item['category'] = "Food"
-    #         item['animal'] = "Dog"
-            
-    #         yield item
-    
+ 
     def parse(self, response):
         for product in response.css('div.in'):
             item = ProjectPetsItem()
@@ -45,3 +31,47 @@ class PethappySpider(scrapy.Spider):
         next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+class PethappyCatFoodSpider(scrapy.Spider):
+    name = 'pethappy_cat_food'
+    allowed_domains = ['https://www.pethappy.cl']
+
+    start_urls = [
+        'https://www.pethappy.cl/gatos-2/alimentos?page=%s' % page for page in range(1, 6)
+    ]
+ 
+    def parse(self, response):
+        for product in response.css('div.in'):
+            item = ProjectPetsItem()
+
+            item['name'] = product.css('h1 a::text').extract_first()
+            item['href'] = "https://www.pethappy.cl" + product.css('p.foto a::attr(href)').extract()[0]
+            price = product.css('p.precio::text').extract()[0]
+            item['price'] = int(re.sub("[A-z$,.]", "", price).strip())
+            item['image_href'] = product.css('p a img::attr(src)').extract()[0]
+            item['store'] = "Pet Happy"
+            item['category'] = "Food"
+            item['animal'] = "Cat"
+            item['date'] = datetime.today()
+            item['date_str'] = item['date'].strftime('%Y-%m-%d')
+            
+            yield item
+    
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
+    # def parse(self, response):
+    #     for product in response.xpath('.//div[contains(@class, "in")]'):
+    #         item = ProjectPetsItem()
+
+    #         item['name'] = product.xpath('//h1//a/text()').extract()
+    #         item['href'] = product.xpath('//p//a/@href').extract()
+    #         item['price'] = product.xpath('//p[contains(@class, "precio")]/text()').extract()
+    #         item['image_href'] = product.xpath('//p//a//img/@src').extract()
+    #         item['store'] = "Pet Happy"
+    #         item['category'] = "Food"
+    #         item['animal'] = "Dog"
+            
+    #         yield item
