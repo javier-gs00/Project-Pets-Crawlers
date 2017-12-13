c3b70bd5867b88ccc5b05f604ddbfb5a7538a02c
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-12 21:51:54 -0300

    Added Day Mascotas general website crawler. Added parse_name method to utils

--- project_pets/project_pets/spiders/daymascotas.py
+++ project_pets/project_pets/spiders/daymascotas.py
@@ -1,12 +1,11 @@
 """
 Module containing the spiders for http://www.daymascotas.cl
 """
-import re
 from datetime import datetime
 import scrapy
 from project_pets.items import ProjectPetsItem
 
-from project_pets.spiders.utils import parse_price
+from project_pets.spiders.utils import parse_price, parse_name
 
 
 class DaymascotasSpider(scrapy.Spider):
@@ -14,47 +13,32 @@ class DaymascotasSpider(scrapy.Spider):
     name = 'daymascotas'
     allowed_domains = ['daymascotas.cl']
     
-
     def start_requests(self):
-        urls_dog_food = [
-            'http://www.daymascotas.cl/categoria-producto/alimentos-perro/page/%s/' % page for page in range(1, 9)
-        ]
-        urls_dog_meds = [
-            'http://www.daymascotas.cl/perros-2/medicamentos-2?page=%s' % page for page in range(1, 3)
-        ]
-        urls_dog_accs = [
-            'http://www.daymascotas.cl/perros-2/accesorios?page=%s' % page for page in range(1, 8)
-        ]
-        urls_cat_food = [
-            'http://www.daymascotas.cl/gatos-2/alimentos?page=%s' % page for page in range(1, 6)
-        ]
-        urls_cat_meds = [
-            'http://www.daymascotas.cl/gatos-2/medicamentos-1?page=%s' % page for page in range(1, 3)
-        ]
-        urls_cat_accs = [
-            'http://www.daymascotas.cl/gatos-2/accesorios?page=%s' % page for page in range(1, 7)
-        ]
+        urls_dog_food = ['http://daymascotas.cl/categoria-producto/alimentos-perro/page/%s/' % page for page in range(1, 9)]
+        urls_cat_food = ['http://daymascotas.cl/categoria-producto/alimentos-gato/page/%s/' % page for page in range(1, 5)]
+        urls_meds = ['http://daymascotas.cl/categoria-producto/medicamentos-drag-pharma/page/%s/' % page for page in range(1, 7)]
+        urls_accs = ['http://daymascotas.cl/categoria-producto/accesorios/page/%s/' % page for page in range(1, 3)]
         urls = urls_dog_food    \
-                + urls_dog_meds \
-                + urls_dog_accs \
                 + urls_cat_food \
-                + urls_cat_meds \
-                + urls_cat_accs
+                + urls_meds \
+                + urls_accs
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
  
     def parse(self, response):
-        url_animal = response.url.split("/")[5]
-        url_category = response.url.split("/")[5]
+        url_animal = response.url.split("/")[4]
+        url_category = response.url.split("/")[4]
 
         item_animal = ''
         item_category = ''
 
-        if url_animal == 'perro':
+        if url_animal.count('perro'):
             item_animal = 'dog'
-        elif url_animal == 'gatos-2':
+        elif url_animal.count('gato'):
             item_animal = 'cat'
+        else:
+            item_animal = 'unspecified'
         
         if url_category.count('alimentos') > 0:
             item_category = 'food'
@@ -63,13 +47,19 @@ class DaymascotasSpider(scrapy.Spider):
         elif url_category.count('accesorios') > 0:
             item_category = 'accessory'
 
-        for product in response.css('div.in'):
+        for product in response.selector.css('li.show-links-onimage'):
             item = ProjectPetsItem()
 
-            item['name'] = product.css('h1 a::text').extract_first()
-            item['href'] = "http://www.daymascotas.cl" + product.css('p.foto a::attr(href)').extract()[0]
-            item['price'] = self.parse_price(product.css('p.precio::text').extract()[0])
-            item['image_href'] = product.css('p a img::attr(src)').extract()[0]
+            item['name'] = parse_name(product.css('h2::text').extract()[0])
+            item['href'] = product.css('a::attr(href)').extract()[0]
+
+            if product.css('del').extract_first(default='not-found') != 'not-found':
+                price = product.css('span.price ins span.woocommerce-Price-amount::text').extract()[0]
+            else:
+                price = product.css('.woocommerce-Price-amount::text').extract()[0]
+            item['price'] = parse_price(price)
+
+            item['image_href'] = product.css('img::attr(src)').extract()[0]
             item['store'] = "Day Mascotas"
             item['category'] = item_category
             item['animal'] = item_animal
@@ -81,30 +71,19 @@ class DaymascotasSpider(scrapy.Spider):
         next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
-    
-    # @staticmethod
-    # def parse_price(work_str):
-    #     """ Recieves a string and removes everything except the numbers to transform it to a integer """
-    #     return int(re.sub('[A-z$,.]', '', work_str).strip())
 
 class DaymascotasDogFoodSpider(scrapy.Spider):
     """ Spider only for the dog food pages """
     name = 'daymascotas_dog_food'
     allowed_domains = ['http://daymascotas.cl']
 
-    start_urls = [
-        'http://daymascotas.cl/categoria-producto/alimentos-perro/page/%s/' % page for page in range(1, 9)
-    ]
-    # start_urls = [
-    #     'http://daymascotas.cl/categoria-producto/alimentos-perro/'
-    # ]
+    start_urls = ['http://daymascotas.cl/categoria-producto/alimentos-perro/page/%s/' % page for page in range(1, 9)]
 
- 
     def parse(self, response):
         for product in response.selector.css('li.show-links-onimage'):
             item = ProjectPetsItem()
 
-            item['name'] = product.css('h2::text').extract()[0]
+            item['name'] = parse_name(product.css('h2::text').extract()[0])
             item['href'] = product.css('a::attr(href)').extract()[0]
 
             if product.css('del').extract_first(default='not-found') != 'not-found':
@@ -119,85 +98,25 @@ class DaymascotasDogFoodSpider(scrapy.Spider):
             item['animal'] = "dog"
             item['date'] = datetime.today()
             item['date_str'] = item['date'].strftime('%Y-%m-%d')
-            
+
             yield item
-    
+
         next_page = response.css('.next::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
-
-# class DaymascotasDogMedSpider(scrapy.Spider):
-#     name = 'daymascotas_dog_meds'
-#     allowed_domains = ['http://daymascotas.cl']
-
-#     start_urls = [
-#         'http://www.daymascotas.cl/perros-2/medicamentos-2?page=%s' % page for page in range(1, 3)
-#     ]
- 
-#     def parse(self, response):
-#         for product in response.css('div.in'):
-#             item = ProjectPetsItem()
-
-#             item['name'] = product.css('h1 a::text').extract_first()
-#             item['href'] = "http://www.daymascotas.cl" + product.css('p.foto a::attr(href)').extract()[0]
-#             price = product.css('p.precio::text').extract()[0]
-#             item['price'] = int(re.sub("[A-z$,.]", "", price).strip())
-#             item['image_href'] = product.css('p a img::attr(src)').extract()[0]
-#             item['store'] = "Day Mascotas"
-#             item['category'] = "medicine"
-#             item['animal'] = "dog"
-#             item['date'] = datetime.today()
-#             item['date_str'] = item['date'].strftime('%Y-%m-%d')
-            
-#             yield item
-    
-#         next_page = response.css('li.next a::attr(href)').extract_first()
-#         if next_page is not None:
-#             yield response.follow(next_page, callback=self.parse)
-
-# class DaymascotasDogAccSpider(scrapy.Spider):
-#     name = 'daymascotas_dog_accessories'
-#     allowed_domains = ['http://www.daymascotas.cl']
-
-#     start_urls = [
-#         'http://www.daymascotas.cl/perros-2/accesorios?page=%s' % page for page in range(1, 8)
-#     ]
- 
-#     def parse(self, response):
-#         for product in response.css('div.in'):
-#             item = ProjectPetsItem()
-
-#             item['name'] = product.css('h1 a::text').extract_first()
-#             item['href'] = "http://www.daymascotas.cl" + product.css('p.foto a::attr(href)').extract()[0]
-#             price = product.css('p.precio::text').extract()[0]
-#             item['price'] = int(re.sub("[A-z$,.]", "", price).strip())
-#             item['image_href'] = product.css('p a img::attr(src)').extract()[0]
-#             item['store'] = "Day Mascotas"
-#             item['category'] = "accessories"
-#             item['animal'] = "dog"
-#             item['date'] = datetime.today()
-#             item['date_str'] = item['date'].strftime('%Y-%m-%d')
-            
-#             yield item
-    
-#         next_page = response.css('li.next a::attr(href)').extract_first()
-#         if next_page is not None:
-#             yield response.follow(next_page, callback=self.parse)
 
 class DaymascotasCatFoodSpider(scrapy.Spider):
     """ Spider only for the dog food pages """
     name = 'daymascotas_cat_food'
     allowed_domains = ['http://daymascotas.cl']
 
-    start_urls = [
-        'http://daymascotas.cl/categoria-producto/alimentos-gato/page/%s/' % page for page in range(1, 5)
-    ]
- 
+    start_urls = ['http://daymascotas.cl/categoria-producto/alimentos-gato/page/%s/' % page for page in range(1, 5)]
+
     def parse(self, response):
         for product in response.selector.css('li.show-links-onimage'):
             item = ProjectPetsItem()
 
-            item['name'] = product.css('h2::text').extract()[0]
+            item['name'] = parse_name(product.css('h2::text').extract()[0])
             item['href'] = product.css('a::attr(href)').extract()[0]
 
             if product.css('del').extract_first(default='not-found') != 'not-found':
@@ -219,60 +138,68 @@ class DaymascotasCatFoodSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
-# class DaymascotasCatMedSpider(scrapy.Spider):
-#     name = 'daymascotas_cat_meds'
-#     allowed_domains = ['http://www.daymascotas.cl']
+class DaymascotasMedSpider(scrapy.Spider):
+    """ Spider only for the medicine pages """
+    name = 'daymascotas_meds'
+    allowed_domains = ['http://daymascotas.cl']
 
-#     start_urls = [
-#         'http://www.daymascotas.cl/gatos-2/medicamentos-1?page=%s' % page for page in range(1, 3)
-#     ]
+    start_urls = ['http://daymascotas.cl/categoria-producto/medicamentos-drag-pharma/page/%s/' % page for page in range(1, 7)]
  
-#     def parse(self, response):
-#         for product in response.css('div.in'):
-#             item = ProjectPetsItem()
+    def parse(self, response):
+        for product in response.selector.css('li.show-links-onimage'):
+            item = ProjectPetsItem()
 
-#             item['name'] = product.css('h1 a::text').extract_first()
-#             item['href'] = "http://www.daymascotas.cl" + product.css('p.foto a::attr(href)').extract()[0]
-#             price = product.css('p.precio::text').extract()[0]
-#             item['price'] = int(re.sub("[A-z$,.]", "", price).strip())
-#             item['image_href'] = product.css('p a img::attr(src)').extract()[0]
-#             item['store'] = "Day Mascotas"
-#             item['category'] = "medicine"
-#             item['animal'] = "cat"
-#             item['date'] = datetime.today()
-#             item['date_str'] = item['date'].strftime('%Y-%m-%d')
+            item['name'] = parse_name(product.css('h2::text').extract()[0])
+            item['href'] = product.css('a::attr(href)').extract()[0]
+
+            if product.css('del').extract_first(default='not-found') != 'not-found':
+                price = product.css('span.price ins span.woocommerce-Price-amount::text').extract()[0]
+            else:
+                price = product.css('.woocommerce-Price-amount::text').extract()[0]
+            item['price'] = parse_price(price)
+
+            item['image_href'] = product.css('img::attr(src)').extract()[0]
+            item['store'] = "Day Mascotas"
+            item['category'] = "medicine"
+            item['animal'] = "unspecified"
+            item['date'] = datetime.today()
+            item['date_str'] = item['date'].strftime('%Y-%m-%d')
             
-#             yield item
+            yield item
     
-#         next_page = response.css('li.next a::attr(href)').extract_first()
-#         if next_page is not None:
-#             yield response.follow(next_page, callback=self.parse)
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
-# class DaymascotasCatAccSpider(scrapy.Spider):
-#     name = 'daymascotas_cat_accessories'
-#     allowed_domains = ['http://www.daymascotas.cl']
+class DaymascotasAccSpider(scrapy.Spider):
+    """ Spider only for the accessories pages """
+    name = 'daymascotas_accessories'
+    allowed_domains = ['http://www.daymascotas.cl']
 
-#     start_urls = [
-#         'http://www.daymascotas.cl/gatos-2/accesorios?page=%s' % page for page in range(1, 7)
-#     ]
+    start_urls = ['http://daymascotas.cl/categoria-producto/accesorios/page/%s/' % page for page in range(1, 3)]
  
-#     def parse(self, response):
-#         for product in response.css('div.in'):
-#             item = ProjectPetsItem()
+    def parse(self, response):
+        for product in response.selector.css('li.show-links-onimage'):
+            item = ProjectPetsItem()
 
-#             item['name'] = product.css('h1 a::text').extract_first()
-#             item['href'] = "http://www.daymascotas.cl" + product.css('p.foto a::attr(href)').extract()[0]
-#             price = product.css('p.precio::text').extract()[0]
-#             item['price'] = int(re.sub("[A-z$,.]", "", price).strip())
-#             item['image_href'] = product.css('p a img::attr(src)').extract()[0]
-#             item['store'] = "Day Mascotas"
-#             item['category'] = "accessories"
-#             item['animal'] = "cat"
-#             item['date'] = datetime.today()
-#             item['date_str'] = item['date'].strftime('%Y-%m-%d')
+            item['name'] = parse_name(product.css('h2::text').extract()[0])
+            item['href'] = product.css('a::attr(href)').extract()[0]
+
+            if product.css('del').extract_first(default='not-found') != 'not-found':
+                price = product.css('span.price ins span.woocommerce-Price-amount::text').extract()[0]
+            else:
+                price = product.css('.woocommerce-Price-amount::text').extract()[0]
+            item['price'] = parse_price(price)
+
+            item['image_href'] = product.css('img::attr(src)').extract()[0]
+            item['store'] = "Day Mascotas"
+            item['category'] = "accessory"
+            item['animal'] = "unspecified"
+            item['date'] = datetime.today()
+            item['date_str'] = item['date'].strftime('%Y-%m-%d')
             
-#             yield item
+            yield item
     
-#         next_page = response.css('li.next a::attr(href)').extract_first()
-#         if next_page is not None:
-#             yield response.follow(next_page, callback=self.parse)
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
--- project_pets/project_pets/spiders/utils.py
+++ project_pets/project_pets/spiders/utils.py
@@ -8,3 +8,18 @@ def parse_price(work_str):
     """ Receives a string and removes everything except the numbers
         to transform it to a integer """
     return int(re.sub('[A-z$,.]', '', work_str).strip())
+
+def parse_name(work_str):
+    """ Removes unwanted characters from scraped names for data
+        uniformity """
+    substitutions = [
+        ('"', ''),
+        ("'", ''),
+        ('.', ' '),
+        ('-', ' ')
+    ]
+
+    for search, replacement in substitutions:
+        work_str = work_str.replace(search, replacement)
+
+    return work_str
