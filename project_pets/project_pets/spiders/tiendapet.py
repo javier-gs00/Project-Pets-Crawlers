ad8a296e99a67488d63746a1aaf62c8dc9623155
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-15 16:00:54 -0300

    Added Tienda Pet food, med & acc general crawler

--- project_pets/project_pets/spiders/tiendapet.py
+++ project_pets/project_pets/spiders/tiendapet.py
@@ -7,18 +7,73 @@ from project_pets.items import ProjectPetsItem
 from project_pets.spiders.utils import parse_price, parse_name
 
 
+class TiendapetSpider(scrapy.Spider):
+    """ Spider for the food, med and accessory pages """
+    name = 'tiendapet'
+    allowed_domains = ['https://www.tiendapet.cl']
+
+    def start_requests(self):
+        urls_dog_food = ['https://www.tiendapet.cl/catalogo/perro/alimentos/%s' % page for page in range(1,45)]
+        urls_dog_meds = ['https://www.tiendapet.cl/catalogo/perro/farmacia/%s' % page for page in range(1,17)]
+        urls_dog_acc = ['https://www.tiendapet.cl/catalogo/perro/accesorios/%s' % page for page in range(1,18)]
+        urls_dog_toys = ['https://www.tiendapet.cl/catalogo/perro/juguetes/%s' % page for page in range(1,10)]
+        urls_dog_accs = urls_dog_acc + urls_dog_toys
+        urls_cat_food = ['https://www.tiendapet.cl/catalogo/gato/alimentos/%s' % page for page in range(1,20)]
+        urls_cat_meds = ['https://www.tiendapet.cl/catalogo/gato/farmacia/%s' % page for page in range(1,9)]
+        urls_cat_acc = ['https://www.tiendapet.cl/catalogo/gato/accesorios/%s' % page for page in range(1,10)]
+        urls_cat_toys = ['https://www.tiendapet.cl/catalogo/gato/juguetes/%s' % page for page in range(1,9)]
+        urls_cat_accs = urls_cat_acc + urls_cat_toys
+        urls = urls_dog_food    \
+                + urls_dog_meds \
+                + urls_dog_accs \
+                + urls_cat_food \
+                + urls_cat_meds \
+                + urls_cat_accs
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+        
+    def parse(self, response):
+        for product in response.selector.css('div.block-producto'):
+            scraped_product = Product()
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
+            scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
+            scraped_product.price = product.css('table').extract()[0]
+            scraped_product.image_href = product.css('img::attr(src)').extract()[0]
+            scraped_product.category = "food"
+            scraped_product.animal = "dog"
+
+            product_list = parse_price_table(scraped_product)
+
+            for final_product in product_list:
+                item = ProjectPetsItem()
+                item['name'] = final_product.name
+                item['href'] = final_product.href
+                item['price'] = final_product.price
+                item['image_href'] = final_product.image_href
+                item['store'] = final_product.store
+                item['category'] = final_product.category
+                item['animal'] = final_product.animal
+                item['date'] = final_product.date
+                item['date_str'] = final_product.date_str
+
+                yield item
+
+        next_page = response.css('a.fa-chevron-right::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
 class TiendapetDogFoodSpider(scrapy.Spider):
     """ Spider only for the dog food pages """
     name = 'tiendapet_dog_food'
     allowed_domains = ['https://www.tiendapet.cl']
 
-    # start_urls = ['https://www.tiendapet.cl/catalogo/perro/alimentos/%s' % page for page in range(1,45)]
-    start_urls = ['https://www.tiendapet.cl/catalogo/perro/alimentos']
+    start_urls = ['https://www.tiendapet.cl/catalogo/perro/alimentos/%s' % page for page in range(1,45)]
+    # start_urls = ['https://www.tiendapet.cl/catalogo/perro/alimentos']
 
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            # scraped_product.name = parse_name(product.css('h5::text').extract()[0])
             scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
@@ -57,7 +112,7 @@ class TiendapetDogMedSpider(scrapy.Spider):
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            scraped_product.name = parse_name(product.css('h5::text').extract()[0])
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
             scraped_product.image_href = product.css('img::attr(src)').extract()[0]
@@ -102,7 +157,7 @@ class TiendapetDogAccSpider(scrapy.Spider):
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            scraped_product.name = parse_name(product.css('h5::text').extract()[0])
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
             scraped_product.image_href = product.css('img::attr(src)').extract()[0]
@@ -140,7 +195,7 @@ class TiendapetCatFoodSpider(scrapy.Spider):
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            scraped_product.name = product.css('h5::text').extract()[0]
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
             scraped_product.image_href = product.css('img::attr(src)').extract()[0]
@@ -172,13 +227,13 @@ class TiendapetCatMedSpider(scrapy.Spider):
     name = 'tiendapet_cat_med'
     allowed_domains = ['https://www.tiendapet.cl']
 
-    # start_urls = ['https://www.tiendapet.cl/catalogo/gato/farmacia/%s' % page for page in range(1,9)]
-    start_urls = ['https://www.tiendapet.cl/catalogo/gato/farmacia']
+    start_urls = ['https://www.tiendapet.cl/catalogo/gato/farmacia/%s' % page for page in range(1,9)]
+    # start_urls = ['https://www.tiendapet.cl/catalogo/gato/farmacia']
 
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            scraped_product.name = product.css('h5::text').extract()[0]
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
             scraped_product.image_href = product.css('img::attr(src)').extract()[0]
@@ -223,7 +278,7 @@ class TiendapetCatAccSpider(scrapy.Spider):
     def parse(self, response):
         for product in response.selector.css('div.block-producto'):
             scraped_product = Product()
-            scraped_product.name = product.css('h5::text').extract()[0]
+            scraped_product.name = parse_name(product.css('a.catalogo_click_detail::attr(href)').extract_first().split('/')[6])
             scraped_product.href = product.css('a.catalogo_click_detail::attr(href)').extract()[0]
             scraped_product.price = product.css('table').extract()[0]
             scraped_product.image_href = product.css('img::attr(src)').extract()[0]
